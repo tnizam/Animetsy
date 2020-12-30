a2d295db399d9e321a380230ae273e91031500bd
tnizam <tahmina.nizam94@gmail.com>
2020-12-29 22:37:23 -0500

    reducer for cart finished

--- frontend/reducers/cart_item_reducer.js
+++ frontend/reducers/cart_item_reducer.js
@@ -1,16 +1,19 @@
 import {} from '../actions/cart_item_actions'
 
 const cartItemReducer = (oldState = {}, action) => {
+    let nextState = Object.assign({}, oldState);
     Object.freeze(oldState);
     switch(action.type) {
         case RECEIVE_CART_ITEMS:
             return action.cartItems;
         case RECEIVE_CART_ITEM:
-            let nextState = Object.assign({}, oldState);
             nextState[action.cartItem.id] = action.cartItem;
             return nextState;
         case REMOVE_CART_ITEM:
-            
+            delete nextState[action.cartItem.id];
+            return nextState;
+        default:
+            return state;
     }
 }
 
