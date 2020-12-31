12f2c4e6b04cbfe8294d01ba9c49e559b6cbe564
tnizam <tahmina.nizam94@gmail.com>
2020-12-30 19:45:37 -0500

    container CART created

--- frontend/components/Cart Item/cart_item_container.js
+++ frontend/components/Cart Item/cart_item_container.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import CartItem from './cart_item';
 import {} from '../../actions/cart_item_actions';
+import { destroyCartItem, fetchCartItems, updateCartItem } from '../../util/cart_item_api_util';
 
 const mSTP = (state) => {
     return {
@@ -11,7 +12,9 @@ const mSTP = (state) => {
 
 const mDTP = dispatch => {
     return {
-        
+        fetchCartItems: () => dispatch(fetchCartItems()),
+        updateCartItem: (cartItem, cartItemId) => dispatch(updateCartItem(cartItem, cartItemId)),
+        destroyCartItem: (cartItemId) => dispatch(destroyCartItem(cartItemId))
     }
 };
 
